e03797a0d70d242d76d5b61fdfdc505c785c46cb
naeunchan <eunchan0492@naver.com>
2021-02-12 17:24:57 +0900

    21/02/12

---
+++ src/__test__/04/Text.test.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import Text from "../../04/Text";
+
+describe("<Text>", () => {
+  it("renders without crashing", () => {
+    expect(() => {
+      shallow(<Text>test</Text>);
+    }).not.toThrow();
+  });
+
+  it("contains <span>", () => {
+    expect(
+      shallow(<Text>test</Text>)
+        .dive()
+        .find("span"),
+    ).toHaveLength(1);
+  });
+});
